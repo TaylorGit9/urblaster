3563982c3e3dbac10e48bcd9c103f7953c56b5a5
THIBAULT LE CONAN <leconan.thibault@gmail.com>
2018-08-16 22:25:42 +0200

    modif

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -5,13 +5,10 @@ import { TabNavigator, TabBarBottom } from 'react-navigation';
 
 import Colors from '../constants/Colors';
 
-/*
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-*/
 import SettingsScreen from '../screens/SettingsScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import DiscoverScreen from '../screens/DiscoverScreen';
+import ColorScreen from '../screens/ColorScreen';
 
 export default TabNavigator(
   {
@@ -21,16 +18,11 @@ export default TabNavigator(
     Discover:{
       screen:DiscoverScreen,
     },
-    /*
-    Home: {
-      screen: HomeScreen,
-    },
-    Links: {
-      screen: LinksScreen,
-    },
-    */
     Settings: {
       screen: SettingsScreen,
+    },
+    ColorScreen: {
+      screen: ColorScreen,
     },
     
   },
@@ -51,25 +43,15 @@ export default TabNavigator(
             Platform.OS === 'ios'
               ? `ios-planet${focused ? '' : '-outline'}`
               : 'md-planet';
-            /*
-              ? `ios-globe${focused ? '' : '-outline'}`
-              : 'md-globe';
-              */
             break;
-            /*
-          case 'Home':
-            iconName =
-              Platform.OS === 'ios'
-                ? `ios-information-circle${focused ? '' : '-outline'}`
-                : 'md-information-circle';
-            break;
-          case 'Links':
-            iconName = Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link';
-            break;
-            */
           case 'Settings':
             iconName =
               Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options';
+            break;
+          case 'Colors':
+            iconName =
+              Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options';
+            break;
               
         }
         return (
